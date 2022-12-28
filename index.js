04e48adff3d79690ffae036c9bbd46bccf0b05e7
Robin Hossain <robinhossain5261@gmail.com>
2022-12-29 01:45:57 +0600

    media task collection complete

--- index.js
+++ index.js
@@ -14,11 +14,32 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nbqc6.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+async function run() {
+    try {
+        const taskOptionCollection = client.db('Hi-task').collection('taskCollection');
+        const mediaTasksCollection = client.db('Hi-task').collection('mediaTasks');
+
+
+        app.post('/mediaTasks', async (req, res) => {
+            const mediaTask = req.body;
+            const result = await mediaTasksCollection.insertOne(mediaTask);
+            res.send(result)
+        })
+
+        app.get('/mediaTasks', async (req, res) => {
+            const query = {};
+            const tasks = await mediaTasksCollection.find(query).toArray();
+            res.send(tasks);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
 
 
 
